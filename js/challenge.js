8c501fc925da7d14f141286fe35613ef7b402587
Tirsa St. Fort <lifelovelori@gmail.com>
2023-02-18 15:22:35 -0800

    Completed lab

--- js/challenge.js
+++ js/challenge.js
@@ -8,20 +8,54 @@ function decreaseCounter () {
     document.getElementById("counter").innerHTML = count - 1
 }
 
+const numbersObj = {}
 
 function likeCounter () {
-    let count = (document.getElementById("counter").innerHTML)
-    let numberOfLikes = 0
-    numberOfLikes += 1
+    let count = Number(document.getElementById("counter").innerHTML)
     const ul = document.getElementsByClassName("likes")
-    const li = document.createElement("li")
-    li.innerText = (`${count} has been liked ${numberOfLikes} time`)
-    ul[0].appendChild(li)
+    if (numbersObj[count] === undefined) {
+        const li = document.createElement("li")
+        li.setAttribute("id", count)
+        ul[0].appendChild(li)
+        numbersObj[count] = 0
+        numbersObj[count]++
+        li.innerText = (`${count} has been liked 1 time.`)
+    } else {
+        let liCount = document.getElementById(count)
+        numbersObj[count]++
+        liCount.innerText = (`${count} has been liked ${numbersObj[count]} times`)
+    }
 }
 
+function pauseCounter () {
+    if (document.getElementById("pause").innerText === "pause") {
+        document.getElementById("plus").disabled = true
+        document.getElementById("minus").disabled = true
+        document.getElementById("heart").disabled = true
+        document.getElementById("pause").innerText = "resume"
+        clearInterval(counter)
+    } else {
+        document.getElementById("plus").disabled = false
+        document.getElementById("minus").disabled = false
+        document.getElementById("heart").disabled = false
+        document.getElementById("pause").innerText = "pause"
+        setInterval(increaseCounter, 1000)
+    }
+}
 
-setInterval(increaseCounter, 1000)
+function leaveComment (event) {
+    event.preventDefault()
+    let comment = document.getElementById("comment-input").value
+    if (comment !== ""){
+        document.getElementById("list").innerHTML += comment
+        document.getElementById("list").innerHTML += "<br>"
+    }   
+}
+
+const counter = setInterval(increaseCounter, 1000)
 
 document.getElementById("plus").addEventListener("click", increaseCounter)
 document.getElementById("minus").addEventListener("click", decreaseCounter)
 document.getElementById("heart").addEventListener("click", likeCounter)
+document.getElementById("pause").addEventListener("click", pauseCounter)
+document.getElementById("submit").addEventListener("click", leaveComment)
